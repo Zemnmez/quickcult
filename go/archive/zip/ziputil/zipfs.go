3a55756b2a42197f95ccb5f841505f0eb570b686
zemnmez <zemnmez@googlemail.com>
2021-08-01 19:15:49 +0100

    a

---
+++ go/archive/zip/ziputil/zipfs.go
@@ -0,0 +1,35 @@
+package ziputil
+
+import (
+	"archive/zip"
+	"io"
+	"io/fs"
+)
+
+func ZipFs(w io.Writer, fs fs.FS) (err error) {
+	z := zip.NewWriter(w)
+
+	defer z.Close()
+
+	return fs.WalkDir(fs, ".", func(path string, d fs.DirEntry, err error) error {
+		if fs.IsDir() {
+			return nil
+		}
+		dst, err := z.Create(path)
+		if err != nil {
+			return err
+		}
+
+		src, err := fs.Open(path)
+		if err != nil {
+			return err
+		}
+		defer src.Close()
+
+		_, err = io.Copy(dst, src)
+		if err != nil {
+			return nil
+		}
+
+	})
+}
