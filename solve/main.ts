dbcee4e208600d6ae73203b2d94e12868c8b5948
zemnmez <zemnmez@gmail.com>
2021-08-19 01:25:24 +0000

    begin the search :)

--- solve/main.ts
+++ solve/main.ts
@@ -1,9 +1,10 @@
-import core from 'gen/core_en_ts';
-import * as Core from 'gen/core_en_ts';
+import * as cultist from './types';
+import fs from 'fs';
 
-type ElementIdCache = Map<string, Core.Element>
 
-export function elementById(id: string, core: Core.Core, cache?: ElementIdCache): [Core.Element | undefined, ElementIdCache] {
+type ElementIdCache = Map<string, cultist.Element>
+
+export function elementById(id: string, core: cultist.Core, cache?: ElementIdCache): [cultist.Element | undefined, ElementIdCache] {
     if (cache === undefined) {
         cache = new Map();
         for (const element of core.elements) {
@@ -14,13 +15,49 @@ export function elementById(id: string, core: Core.Core, cache?: ElementIdCache)
     return [cache.get(id), cache ]
 }
 
-export function legacyInfo(l: Core.Legacy): string {
-    return JSON.stringify(l)
+export function mustElementById(id: string, core: cultist.Core, cache?: ElementIdCache): [cultist.Element, ElementIdCache] {
+    const [el, c ] = elementById(id, core, cache);
+    if (el === undefined) throw new Error(`Cannot find element ${id}`);
+    return [ el, c ];
+
 }
 
+function* duplicate<T>(i: Iterable<[T, number]>) {
+    for (const [v, n] of i) {
+        for (let i = 0; i < n; i++) {
+            yield v;
+        }
+    }
+}
+
+export function elementsByEffects(effects: Record<string, number>, core: cultist.Core, cache?: ElementIdCache): cultist.Element[] {
+    const elementsAndCounts = Object.entries(effects).map(([id, count]): [ cultist.Element, number ] => {
+        let el: cultist.Element;
+        ([el, cache] = mustElementById(id, core, cache))
+        return [el, count];
+    });
+
+    const rets =  [...duplicate(elementsAndCounts)];
+    return rets;
+}
+
+interface BoardState {
+    elements?: cultist.Element[]
+    verbs?: cultist.Verb[]
+}
+
+export function initialBoardStateFromLegacy(l: cultist.Legacy, core: cultist.Core, cache?: ElementIdCache): BoardState {
+    return {
+        elements: l.effects !== undefined? elementsByEffects(l.effects, core, cache): undefined
+    }
+}
+
+
 export const Main = async () => {
+    const core: cultist.Core = JSON.parse((await fs.promises.readFile('gen/core_en.json')).toString('utf-8'));
+    let elementIdCache;
     for (const legacy of core.legacies) {
-        console.log(legacyInfo(legacy as any))
+        console.log(initialBoardStateFromLegacy(legacy, core));
     }
 }
 
