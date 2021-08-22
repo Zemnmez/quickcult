df12e6f8a141f60db8235e6486765f9d4a8c3857
zemnmez <zemnmez@gmail.com>
2021-08-22 23:47:08 +0000

    pattern matching

--- typescript/match.ts
+++ typescript/match.ts
@@ -1,17 +1,21 @@
 export interface Match<I, O> {
-	<I2, IM extends I2, O2>(t: (v: I2) => v is IM, f: (v: IM) => O2): Match<I & I2, O | O2>;
+	<I2, IM extends I2, O2>(t: (v: I2) => v is IM, f: (v: IM) => O2): Match<
+		I & I2,
+		O | O2
+	>;
 	match(v: I): O;
 }
 
 export function pattern<I, O>(fb: (i: I) => O): Match<I, O> {
-	function ret<I2, IM extends I2, O2>(t: (v: I2) => v is IM, f: (v: IM) => O2): Match<I & I2, O | O2> {
-		return pattern<I&I2,O|O2>((i: I & I2) => t(i)? f(i): fb(i))
+	function ret<I2, IM extends I2, O2>(
+		t: (v: I2) => v is IM,
+		f: (v: IM) => O2
+	): Match<I & I2, O | O2> {
+		return pattern<I & I2, O | O2>((i: I & I2) => (t(i) ? f(i) : fb(i)));
 	}
 
-	ret.match = (v: I): O => fb(v)
+	ret.match = (v: I): O => fb(v);
 	return ret as any;
 }
-
-
 
 export default pattern;
--- typescript/match_test.ts
+++ typescript/match_test.ts
@@ -1,14 +1,22 @@
 import pattern from '//typescript/match';
 
 test('match', () => {
-	const match = pattern(() => undefined)
-		((v: { name: string }): v is {name: 'john'}  => v.name == 'john', () => 'hi john!' as const )
-		((v: { age: number }): v is {age: 20}  => v.age == 20, () => 'you are 20 years old' as const);
+	const match = pattern(() => undefined)(
+		(v: { name: string }): v is { name: 'john' } => v.name == 'john',
+		() => 'hi john!' as const
+	)(
+		(v: { age: number }): v is { age: 20 } => v.age == 20,
+		() => 'you are 20 years old' as const
+	);
 
-	expect(match.match({ name: 'whatever', age: 10})).toEqual(undefined);
-	expect(match.match({ name: 'john', age: 10})).toEqual('hi john!');
-	expect(match.match({ name: 'somethign', age: 20})).toEqual('you are 20 years old');
+	expect(
+		match.match({ name: 'whatever' as const, age: 10 as const })
+	).toEqual(undefined);
+	expect(match.match({ name: 'john', age: 10 })).toEqual('hi john!');
+	expect(match.match({ name: 'somethign', age: 20 })).toEqual(
+		'you are 20 years old'
+	);
 
 	// @ts-expect-error
-	expect(match.match(2)).toEqual(undefined)
-})
+	expect(match.match(2)).toEqual(undefined);
+});
